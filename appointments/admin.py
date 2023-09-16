21d390ebd7afc4308b555568c860333a96376b43
Ícaro Abreu <136408366+oicaroabreu@users.noreply.github.com>
2023-09-16 18:25:36 -0300

    feat:appointment_admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Appointment, AppointmentPackage, TimeSlot, DayOfWeek
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = [
+        "client",
+        "therapist",
+        "service",
+        "appointment_date_start",
+        "status",
+    ]
+    list_filter = ["status", "service"]
+    search_fields = ["client__name", "therapist__name"]
+    date_hierarchy = "appointment_date_start"
+    ordering = ["-appointment_date_start"]
+
+
+@admin.register(AppointmentPackage)
+class AppointmentPackageAdmin(admin.ModelAdmin):
+    list_display = ["name", "therapist", "discounted_price"]
+    search_fields = ["name", "therapist__name"]
+    filter_horizontal = ["appointments"]
+
+
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ["start_time", "end_time"]
+
+
+@admin.register(DayOfWeek)
+class DayOfWeekAdmin(admin.ModelAdmin):
+    list_display = ["day"]
+
