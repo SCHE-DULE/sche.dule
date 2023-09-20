94a706e45510eef3506bee1f0830091c75362e92
Ícaro Abreu <136408366+oicaroabreu@users.noreply.github.com>
2023-09-20 01:56:02 -0300

    refactor/permission_when_save

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,16 +7,27 @@ from .models import Client, SystemUser, Therapist, Speciality
 
 @admin.register(SystemUser)
 class SystemUserAdmin(admin.ModelAdmin):
-    list_display = ["name", "user"]
-    search_fields = ["name", "user"]
-    exclude = ["user"]
+    list_display = ["name", "user_type"]
+    search_fields = ["name", "user_type"]
+    exclude = (
+        'password',
+        'last_login',
+        'date_joined',
+        'is_superuser',
+        'username',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+#        'groups',
+#        'user_permissions'
+    )
 
     def save_model(self, request, obj, form, change):
         if not change:
             if not User.objects.filter(email=f"{obj.email}").exists():
                 try:
-                    print(request)
-                    request.user.baseuser
+                    request.user.id
                 except ObjectDoesNotExist:
                     super().save_model(request, obj, form, change)
                 else:
@@ -28,19 +39,34 @@ class SystemUserAdmin(admin.ModelAdmin):
                 messages.error(request, "Este email já está sendo usado!")
         obj.save()
 
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        # Fetch the SystemUser instance
+        system_user = self.get_object(request, object_id)
+        
+        # Print information about the user
+        print(f"Viewing SystemUser: {system_user.name}, Groups: {system_user.groups.all()}")
+
+        
+        # You can add any additional logic or custom code here
+        
+        return super().change_view(request, object_id, form_url, extra_context)
+
+
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ["name", "user"]
+    list_display = ["name", "email"]
     search_fields = ["name"]
-    exclude = ["user"]
+
+    class Meta:
+        verbose_name = "Cliente"
 
 
 @admin.register(Therapist)
 class TherapistAdmin(admin.ModelAdmin):
-    list_display = ["name", "crm", "avaliability_days", "hours", "rate"]
-    search_fields = ["name", "crm"]
-    exclude = ["user"]
+    list_display = ["name", "crm"]
+    search_fields = ["name", "crm", "specialities"]
+    exclude = []
 
 
 @admin.register(Speciality)
